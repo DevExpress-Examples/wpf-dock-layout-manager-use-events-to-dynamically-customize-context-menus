62fe03c5f78c39a7771b47a91768a7d98855230a
DevExpressExampleBot <fakeEmail>
2023-05-18 13:11:34 +0400

    Source auto update [skip ci]

--- VB/DockManager_MenuCustomization/Window1.xaml.vb
+++ VB/DockManager_MenuCustomization/Window1.xaml.vb
@@ -1,55 +1,42 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Bars
 Imports DevExpress.Xpf.Docking
 
 Namespace DockManager_MenuCustomization
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		#Region "#1"
-		Private Sub dockManager1_ShowingMenu(ByVal sender As Object, ByVal e As DevExpress.Xpf.Docking.Base.ShowingMenuEventArgs)
-			Dim menu As ItemContextMenu = (TryCast(e.Menu, ItemContextMenu))
-			If menu Is Nothing Then
-				Return
-			End If
-			' Action 1 - Insert a new About button at the first position in the menu.
-			Dim actionAddAboutItem As New BarButtonItem()
-			' Specify the position for the button via the InsertBarItemLinkAction.ItemLinkIndex attached property.
-			InsertBarItemLinkAction.SetItemLinkIndex(actionAddAboutItem, 0)
-			actionAddAboutItem.Content = "About..."
-			AddHandler actionAddAboutItem.ItemClick, AddressOf btnAbout_ItemClick
-			' Action 2 - Insert a separator
-			Dim actionAddSeparator As New InsertBarItemLinkAction()
-			actionAddSeparator.ItemLink = New BarItemLinkSeparator()
-			actionAddSeparator.ItemLinkIndex = 1
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-			' Add the actions to the ActionList
-			e.ActionList.Add(actionAddAboutItem)
-			e.ActionList.Add(actionAddSeparator)
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub btnAbout_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-			MessageBox.Show("About window")
-		End Sub
-		#End Region ' #1
-	End Class
+'#Region "#1"
+        Private Sub dockManager1_ShowingMenu(ByVal sender As Object, ByVal e As Base.ShowingMenuEventArgs)
+            Dim menu As ItemContextMenu = TryCast(e.Menu, ItemContextMenu)
+            If menu Is Nothing Then Return
+            ' Action 1 - Insert a new About button at the first position in the menu.
+            Dim actionAddAboutItem As BarButtonItem = New BarButtonItem()
+            ' Specify the position for the button via the InsertBarItemLinkAction.ItemLinkIndex attached property.
+            BarItemLinkActionBase.SetItemLinkIndex(actionAddAboutItem, 0)
+            actionAddAboutItem.Content = "About..."
+            AddHandler actionAddAboutItem.ItemClick, New ItemClickEventHandler(AddressOf btnAbout_ItemClick)
+            ' Action 2 - Insert a separator
+            Dim actionAddSeparator As InsertBarItemLinkAction = New InsertBarItemLinkAction()
+            actionAddSeparator.ItemLink = New BarItemLinkSeparator()
+            actionAddSeparator.ItemLinkIndex = 1
+            ' Add the actions to the ActionList
+            e.ActionList.Add(actionAddAboutItem)
+            e.ActionList.Add(actionAddSeparator)
+        End Sub
+
+        Private Sub btnAbout_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            MessageBox.Show("About window")
+        End Sub
+'#End Region  ' #1
+    End Class
 End Namespace
